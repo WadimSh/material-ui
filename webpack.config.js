c639576cff8bf499e52a2ac6aad7068824c90518
WadimSh <wadim11@yandex.ru>
2023-12-12 10:47:05 +0300

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options:{
+            presets: ['@babel/preset-react']
+          }
         }
       }
     ]
