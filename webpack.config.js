cc69e242afd5b42fd1d2fe6dad01bed26ed399d9
WadimSh <wadim11@yandex.ru>
2023-12-12 10:41:15 +0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,13 @@ module.exports = {
         }
       }
     ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   }
 };
