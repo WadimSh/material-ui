ea6fc9c17dfd6d940f92e5a9e004a6e5c67cd065
WadimSh <wadim11@yandex.ru>
2023-12-18 13:30:45 +0300

    feat: добавлен в сборку компонент textBlock

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Button } from './components/button/button';
 export { default as Heading } from './components/heading/heading';
+export { default as TextBlock } from './components/textBlock/textBlock';
