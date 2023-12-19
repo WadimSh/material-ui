a8e54956b1aa089701bd6dd9ad101cc766ef08b6
WadimSh <wadim11@yandex.ru>
2023-12-19 16:53:53 +0300

    feat: компонент TextInput включен в библиотеку

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './components/button/button';
 export { default as Heading } from './components/heading/heading';
 export { default as TextBlock } from './components/textBlock/textBlock';
+export { default as TextInput } from './components/textInput/textInput';
