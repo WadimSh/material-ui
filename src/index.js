76baf40d9938214131ee7bc29989703766070b86
WadimSh <wadim11@yandex.ru>
2023-12-21 10:30:21 +0300

    feat: компонент radiogroup добавлен в сборку библиотеки

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as Heading } from './components/heading/heading';
 export { default as TextBlock } from './components/textBlock/textBlock';
 export { default as TextInput } from './components/textInput/textInput';
 export { default as CheckBox } from './components/checkBox/checkBox';
+export { default as RadioGroup } from './components/radioGroup/radioGroup';
